f3dc923aec999f1e1e0eb4b7e3af0de54febe77a
lihao <lihao.cuapp.me>
2018-04-10 13:12:47 +0800

    完成

--- assets/js/toapp.js
+++ assets/js/toapp.js
@@ -3,7 +3,7 @@ var tasklist = [
         id: 1,
         name: '吃饭',
         isFinish: false,
-        isEditing: true
+        isEditing: false
     },
     {
         id: 2,
@@ -24,15 +24,9 @@ new Vue({
     data: {
         task: '',
         isSelected: 'all',
-        tasklist:tasklist
+        tasklist: tasklist
     },
-    computed: {},
     methods: {
-        getData: function () {
-            if (localStorage.getItem('tasklist')) {
-                this.tasklist = angular.fromJson(localStorage.getItem('tasklist'))
-            }
-        },
         addTask: function () {
             if (this.task) {
                 this.tasklist.push({
@@ -47,27 +41,19 @@ new Vue({
         destroy: function (index) {
             //传入当前项
             this.tasklist.splice(index, 1);
-        },
-        unCompletedTasks: function () {
-            var num = 0;
-            angular.forEach(this.tasklist, function (item, index) {
-                if (!item.isFinish) {
-                    num++
-                }
-            })
-            return num;
-        },
-        ctrlShow: function () {
-            for (var i = 0; i < this.tasklist.length; i++) {
-                if (this.tasklist[i].isFinish) {
-                    return true;
-                }
-            }
         }, changeAllStatus: function (e) {
-            this.tasklist.forEach(item => item.isFinish=e.target.checked);
+            this.tasklist.forEach(item => item.isFinish = e.target.checked)
         }, edit: function (item) {
-            item.isEditing = true
-        }, cancelModify: function (item) {
+            this.tasklist.forEach(item => item.isEditing = false)
+            ;
+            item.isEditing = true;
+        }, saveEdit: function (item, index, e) {
+            if (!(item.name).trim()) {
+                this.tasklist.splice(index, 1);
+            }
+            item.name = e.target.value
+            item.isEditing = false;
+        }, cancelEdit: function (item) {
             item.isEditing = false;
         }, filterTask: function (task) {
             switch (task) {
@@ -85,12 +71,16 @@ new Vue({
                     break;
             }
         }, clear: function () {
-            for (var i = 0; i < this.tasklist.length; i++) {
-                if (this.tasklist[i].isFinish) {
-                    this.tasklist.splice(i, 1);
-                    i--;
+            //清空已完成 foreach会报错
+            //1
+            for (var i=0;i<this.tasklist.length;i++){
+                if(this.tasklist[i].isFinish){
+                    this.tasklist.splice(i,1);
+                    i--
                 }
             }
+            //2
+           // this.tasklist = this.tasklist.filter(item => !item.isFinish)
         }
     }
 });
