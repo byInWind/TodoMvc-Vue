35d234dbaa4c014a5f95a1ed9330a3e4c3dec56c
lihao <lihao.cuapp.me>
2018-04-11 11:00:20 +0800

    继续完善

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # TodoMvc-Vue
-vue练习todomvc 
-查看效果https://byinwind.github.io/TodoMvc-Vue/.
+## Vue练习todomvc   
+练习Vue,体会与jq的差异。模板来自todomvc,使用了Vue中的基本指令，计算属性，监视数据变化的watch,
+自定义指令，加上了localStorage  
+
+查看效果https://byinwind.github.io/TodoMvc-Vue/ 
--- assets/js/toapp.js
+++ assets/js/toapp.js
@@ -1,29 +1,29 @@
-var tasklist = [
-    {
-        id: 1,
-        name: '吃饭',
-        isFinish: false,
-        isEditing: false
-    },
-    {
-        id: 2,
-        name: '洗澡',
-        isFinish: true,
-        isEditing: false
-    },
-    {
-        id: 3,
-        name: '刷牙',
-        isFinish: true,
-        isEditing: false
-    }
-];
+// var tasklist = [
+//     {
+//         id: 1,
+//         name: '吃饭',
+//         isFinish: false,
+//         isEditing: false
+//     },
+//     {
+//         id: 2,
+//         name: '洗澡',
+//         isFinish: true,
+//         isEditing: false
+//     },
+//     {
+//         id: 3,
+//         name: '刷牙',
+//         isFinish: true,
+//         isEditing: false
+//     }
+// ];
 
-new Vue({
+var app = new Vue({
     el: "#app",
     data: {
         task: '',
-        isSelected: 'all',
+        filterText: 'all',
         tasklist: JSON.parse(window.localStorage.getItem('tasklist') || '[]')
     },
     methods: {
@@ -41,8 +41,6 @@ new Vue({
         destroy: function (index) {
             //传入当前项
             this.tasklist.splice(index, 1);
-            window.localStorage.setItem('tasklist',JSON.stringify(tasklist));
-
         }, changeAllStatus: function (e) {
             this.tasklist.forEach(item => item.isFinish = e.target.checked)
         }, edit: function (item) {
@@ -57,21 +55,6 @@ new Vue({
             item.isEditing = false;
         }, cancelEdit: function (item) {
             item.isEditing = false;
-        }, filterTask: function (task) {
-            switch (task) {
-                case 'all':
-                    this.isSelected = task;
-                    this.condition = '';
-                    break;
-                case 'active':
-                    this.isSelected = task;
-                    this.condition = false;
-                    break;
-                case 'completed':
-                    this.isSelected = task;
-                    this.condition = true;
-                    break;
-            }
         }, clear: function () {
             //清空已完成 foreach会报错
             //1
@@ -96,6 +79,18 @@ new Vue({
         },
         toggleAllStatus:function () {
             return this.tasklist.every( item => item.isFinish )
+        }, filterTask: function () {
+            switch (this.filterText) {
+                case 'active':
+                    return this.tasklist.filter(item => !item.isFinish)
+                    break;
+                case 'completed':
+                    return this.tasklist.filter(item => item.isFinish)
+                    break;
+                default:
+                    return this.tasklist
+                    break;
+            }
         }
     },
     created:function () {
@@ -103,6 +98,7 @@ new Vue({
     },
     watch:{
         tasklist: {
+            //通过watch监视tasklist变化，不用在增删改中分别添加localStorage
             handler: function () {
                 window.localStorage.setItem('tasklist',JSON.stringify(this.tasklist));
             },
@@ -110,3 +106,7 @@ new Vue({
         }
     }
 });
+//也可以这样
+// window.onhashchange = function () {
+//     app.filterText = window.location.hash.substr(2)
+// }
